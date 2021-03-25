a858bd9abece49a0c8d135788659c2682a87f6a9
Ciaddie <ciaddiebb@gmail.com>
2021-03-25 00:18:27 -0700

    attempt at fizzbuzz, suspectingly right answer

--- fizzbuzz/README.md
+++ fizzbuzz/README.md
@@ -5,4 +5,4 @@ given a list of numbers 1-100, organize them into the following categories:
 -if the number is a multiple of 3, label it fizz.
 -if the number is a multiple of 5, label it buzz.
 -if the number is a multiple of both 5 and 3, label it fizzbuzz.
--if the number cannoy be divisible
+-if the number cannot be divisible by either, skip.
---
+++ fizzbuzz/fizzBuzz.js
@@ -0,0 +1,21 @@
+const fizzBuzz = () => {
+  let result = {};
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result[i] = 'FizzBuzz';
+    } else if (i % 3 === 0) {
+      result[i] = 'Fizz';
+    } else if (i % 5 === 0) {
+      result[i] = 'Buzz';
+    } else {
+      result[i] = i;
+    }
+  }
+
+  return result;
+}
+
+let test = fizzBuzz();
+
+console.log(test);
