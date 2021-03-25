1dc4a0d1b2b17cf439c78f2e4c68c9e78d663a43
Ciaddie <ciaddiebb@gmail.com>
2021-03-24 23:51:45 -0700

    working solution for simple integers without trailing 0's

--- reverseInteger/reverse.js
+++ reverseInteger/reverse.js
@@ -1,4 +1,20 @@
 const reverse = (number) => {
+  let stringNum = number.toString();
+  let numsArray = stringNum.split('');
+  let result = [];
 
+  if (stringNum.length === 1) {
+    return Number(stringNum);
+  }
+
+  for (let i = numsArray.length - 1; i >= 0; i--) {
+    result.push(numsArray[i]);
+  }
+
+  result = result.join('');
+
+  return Number(result);
 }
 
+let test1 = 123
+console.log(reverse(test1));
