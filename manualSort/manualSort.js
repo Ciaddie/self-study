9253f09c05ce5ea29f4f391c4957ecad2977ee2d
Ciaddie <ciaddiebb@gmail.com>
2021-05-14 21:18:44 -0700

    i sorted manually so guess who isnt gonna cry today

--- manualSort/manualSort.js
+++ manualSort/manualSort.js
@@ -1,32 +1,28 @@
 const manualSort = (arr) => {
   let result = [];
   let sorted = false;
-
-  // while (sorted === false) {
-  //   let smallest = arr[0];
-  //   let temp = 0;
-  //   for (let i = 0; i < arr.length(); i++) {
-  //     if (smallest > arr[i]) {
-
-  //     }
-  //   }
-  // }
-
-  // return result;
   let temp = 0;
-  let smallest = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (smallest > arr[i]) {
-      smallest = arr[i];
-      temp = arr[0];
-      arr[0] = smallest;
-      arr[i] = temp;
+  while (sorted === false) {
+    let smallest = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (smallest > arr[i]) {
+        smallest = arr[i];
+        temp = arr[0];
+        arr[0] = smallest;
+        arr[i] = temp;
+      }
+    }
+    result.push(arr[0])
+    arr.splice(0, 1);
+
+    if (arr.length === 0) {
+      sorted = true;
     }
   }
-  result.push(arr[0])
-  arr.splice(0, 1);
+
   return result;
 }
 
-console.log(manualSort([10, 5, 6, 7, 2, 9]));
+console.log(manualSort([10, 2, 5, 7, 30 , 4, 77, 55, 38]));
