3d0a00a2372194021efb2a64c64b29e210d87356
Ciaddie <ciaddiebb@gmail.com>
2021-05-20 17:13:37 -0700

    honestly, just adding another commit so that my belt stays green

---
+++ OddishOrEvenish/oddishOrEvenish.js
@@ -0,0 +1,23 @@
+const oddishOrEvenish = (num) => {
+  let splitNum = num.toString().split('');
+  let total = 0;
+
+  for (let i = 0; i < splitNum.length; i++) {
+    let current = parseInt(splitNum[i], 10);
+    total += current;
+  }
+
+  if (total % 2 === 0) {
+    return 'Evenish';
+  } else {
+    return 'Oddish';
+  }
+}
+
+const test1 = oddishOrEvenish(4433);
+const test2 = oddishOrEvenish(43);
+const test3 = oddishOrEvenish(373);
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
